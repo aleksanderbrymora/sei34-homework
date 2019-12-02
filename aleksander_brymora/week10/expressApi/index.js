b1fd7efa8df0644628cfac65eafa077dc5078137
aleksanderbrymora <aleksander.australia@gmail.com>
2019-12-02 17:35:11 +1100

    styling, avatars, displaying on the page

--- aleksander_brymora/week10/expressApi/index.js
+++ aleksander_brymora/week10/expressApi/index.js
@@ -3,6 +3,7 @@ const ejs = require('ejs');
 const axios = require('axios');
 
 const server = express();
+server.use(express.static('public'));
 server.set('view-engine', ejs);
 
 const PORT = 1337;
@@ -10,40 +11,18 @@ const posts = [];
 
 server.get('/', (req, res) => {
     const temp = {
-        image: '',
+        img: '',
         question: '',
         answer: ''
     };
-    axios.get(`https://avatars.dicebear.com/v2/male/${Math.floor(Math.random()*1000)}.svg`).then(response => {
-        console.log(response);
-        temp.img = response.data;
-        axios.get('http://jservice.io/api/random').then(response => {
-            temp.question = response.data.question;
-            temp.answer = response.data.answer;
-            posts.push(temp)
-            console.log(temp);
-            res.render('trivia.ejs', {trivia: posts});
-        });
+    temp.img = `https://avatars.dicebear.com/v2/male/${Math.floor(Math.random()*1000)}.svg`;
+    axios.get('http://jservice.io/api/random').then(response => {
+        temp.question = response.data[0].question;
+        temp.answer = response.data[0].answer;
+        posts.push(temp);
+        console.log(temp)
+        res.render('trivia.ejs', { trivia: posts });
     });
 });
-
-const fetchData = () => {
-    const temp = {
-        image: '',
-        question: '',
-        answer: ''
-    };
-    axios.get(`https://avatars.dicebear.com/v2/male/${Math.floor(Math.random()*1000)}.svg`).then(response => {
-        console.log(response);
-        temp.img = response.data;
-        axios.get('http://jservice.io/api/random').then(response => {
-            temp.question = response.data.question;
-            temp.answer = response.data.answer;
-            posts.push(temp)
-            console.log(temp);
-            res.render('trivia.ejs', {trivia: posts});
-        });
-    });
-};
 
 server.listen(PORT, () => console.log(`Now serving on http://localhost:${ PORT }`));
